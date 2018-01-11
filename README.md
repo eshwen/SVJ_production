26b5cef27c8e9c533752cadd8ca782cb2cf076a6
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-01-11 15:03:03 +0000

    Removed separate script for submission at Imperial and integrated it into run_bunch_batch.sh

--- README.md
+++ README.md
@@ -2,16 +2,13 @@
 
 Sample generation can be run on a batch system (currently Zurich and Imperial College London are supported).
 
-At Zurich, run
+Run
 
 ```bash
-./run_bunch_batch.sh WORKING_DIRECTORY  NUMBER_OF_EVENTS  NUMBER_OF_SEEDS NUMBER_OF_THREADS(to not execute cmsRun leave empty)  MAIL_ADDRESS
+./run_bunch_batch.sh WORKING_DIRECTORY  NUMBER_OF_EVENTS  NUMBER_OF_SEEDS NUMBER_OF_THREADS(to not execute cmsRun leave empty)  SITE  EMAIL_ADDRESS
 ```
 
-At Imperial, run
-
-```bash
-./run_bunch_imperial_batch.sh WORKING_DIRECTORY  NUMBER_OF_EVENTS  NUMBER_OF_SEEDS NUMBER_OF_THREADS(to not execute cmsRun leave empty)
-```
+The argument SITE should be 'zurich' or 'imperial', as the submission commands are slightly different for each batch system.
+The argument EMAIL_ADDRESS is optional.
 
 The default values for the important parameters (alpha\_D, r\_inv and m\_Z') are specified in run\_bunch\_batch.sh with underscores replacing decimals (i.e., 0\_1 = 0.1).
--- run_bunch_batch.sh
+++ run_bunch_batch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-  echo "-----------------------------------------------------------------------------------------------------------------------------------------------
-Usage: ./run_bunch_batch WORKING_DIRECTORY  NUMBER_OF_EVENTS  NUMBER_OF_SEEDS NUMBER_OF_THREADS(to not execute cmsRun leave empty)  MAIL_ADDRESS
------------------------------------------------------------------------------------------------------------------------------------------------"
+  echo "------------------------------------------------------------------------------------------------------------------------------------------------------
+Usage: ./run_bunch_batch.sh WORKING_DIRECTORY  NUMBER_OF_EVENTS  NUMBER_OF_SEEDS NUMBER_OF_THREADS(to not execute cmsRun leave empty)  SITE  EMAIL_ADDRESS
+------------------------------------------------------------------------------------------------------------------------------------------------------"
   exit
 fi
 
@@ -12,13 +12,23 @@ num_ind=1
 n_of_events=$2
 n_of_threads=$4
 n_of_seeds=$3
-MAIL=$5
+site=$5
+MAIL=$6
+
+if [ $site == 'imperial' ]; then
+	queue=hep.q
+elif [ $site == 'zurich' ]; then
+	queue=long.q
+else
+	echo Site not supported. Please choose \'zurich\' or \'imperial\'.
+	exit
+fi
 
 work_space=$(readlink -m $1)
 
-for alpha_D in 01; do
+for alpha_D in 0_1; do # In set_config.sh, alpha_D and r_inv are split by 2nd character. So 0_1 = 0.1
     for m_Z in 3000; do
-	for r_inv in 03; do
+	for r_inv in 0_3; do
 
 	    for seed in  $(seq 1 1 $n_of_seeds); do
 		seed=$(echo 3000+$seed | bc)
@@ -30,9 +40,9 @@ for alpha_D in 01; do
 		fi
 		if [ ! -z $n_of_threads ]; then
 		    if [ -z $MAIL ]; then
-			qsub -N job_alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_${seed} -o logs/ -e logs/ -q long.q -cwd $work_space/run_scripts/run_batch_alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_${seed}.sh
+			qsub -N job_alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_${seed} -o logs/ -e logs/ -q $queue -cwd $work_space/run_scripts/run_batch_alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_${seed}.sh
 		    else
-			qsub -N job_alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_${seed} -o logs/ -e logs/ -q long.q -cwd -m ae -M $MAIL $work_space/run_scripts/run_batch_alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_${seed}.sh
+			qsub -N job_alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_${seed} -o logs/ -e logs/ -q $queue -cwd -m ae -M $MAIL $work_space/run_scripts/run_batch_alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_${seed}.sh
 		    fi
 		fi
 	    done
