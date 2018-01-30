3dadc8db34b97e9193b4e95ca96e9a31ce4d2101
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-01-30 14:22:14 +0100

    Moved writing of submission script to separate file

---
+++ global/write_submission_script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Write the HTCondor submission script for sample generation
+
+work_space=$1
+alpha_D=$2
+m_Z=$3
+r_inv=$4
+seed=$5
+
+echo "# HTCondor submission script
+Universe = vanilla
+executable = $work_space/run_scripts/run_batch_alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_${seed}.sh
+Log        = $work_space/logs/condor_job_${seed}.log
+Output     = $work_space/logs/condor_job_${seed}.out
+Error      = $work_space/logs/condor_job_${seed}.error
+should_transfer_files   = YES
+when_to_transfer_output = ON_EXIT_OR_EVICT
+" > $work_space/run_scripts/condor_submission_${seed}.job
+
+if [[ "$HOSTNAME" == "soolin"* ]]; then                                                                             
+    echo "use_x509userproxy = true" >> $work_space/run_scripts/condor_submission_${seed}.job
+fi
+
+echo "
+# Resource requests (disk storage in kB, memory in MB)
+request_cpus = 1
+request_disk = 1000000
+request_memory = 2500
++MaxRuntime = 7200
+# Number of instances of job to run
+queue 1
+" >> $work_space/run_scripts/condor_submission_${seed}.job
--- run_bunch_batch.sh
+++ run_bunch_batch.sh
@@ -64,24 +64,8 @@ for alpha_D in 0_1; do # In set_config.sh, alpha_D and r_inv are split by 2nd ch
 
 		if [ ! -z $n_of_threads ]; then
 		    if [[ "$HOSTNAME" == "soolin"* ]] || [[ "$HOSTNAME" == "lxplus"* ]]; then
-			echo "# HTCondor submission script
-			Universe = vanilla
-			executable = $work_space/run_scripts/run_batch_alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_${seed}.sh
-			use_x509userproxy = true
-			Log        = $work_space/logs/condor_job_${seed}.log
-			Output     = $work_space/logs/condor_job_${seed}.out
-			Error      = $work_space/logs/condor_job_${seed}.error
-			should_transfer_files   = YES
-			when_to_transfer_output = ON_EXIT_OR_EVICT
-			# Resource requests (disk storage in kB, memory in MB)
-			request_cpus = 1
-			request_disk = 1000000
-			request_memory = 2500
-                        +MaxRuntime = 7200
-			# Number of instances of job to run
-			queue 1
-			" > $work_space/run_scripts/condor_submission_${seed}.job
 
+			$PWD/global/write_submission_script.sh $work_space $alpha_D $m_Z $r_inv $seed
 			condor_submit $work_space/run_scripts/condor_submission_${seed}.job
 			
 		    else
