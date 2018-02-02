57c5f2eff9b3f3c55b37af4bc251d681382ff8fa
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-02-02 16:42:27 +0100

    Fixed paths in hadding script

--- global/write_hadding_script.sh
+++ global/write_hadding_script.sh
@@ -15,8 +15,7 @@ eval \`scramv1 runtime -sh\`
 cd $work_space/output/alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}
 hadd -k alphaD${alpha_D}_mZ${m_Z}_rinv${r_inv}_MINIAOD_final.root *MINIAOD.root
 rm *MINIAOD.root
-cd $work_space/../
-rm hadd_miniAODs.sh
+rm $work_space/hadd_miniAODs.sh
 " > $work_space/hadd_miniAODs.sh
 
 chmod +x $work_space/hadd_miniAODs.sh
